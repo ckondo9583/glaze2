6a23427373fa7f663b1821f1d6da93dc3231884e
ChiakiKondo <154647391+ckondo9583@users.noreply.github.com>
2024-04-02 13:54:49 +0000

    ru-to

--- config/routes.rb
+++ config/routes.rb
@@ -17,33 +17,30 @@ Rails.application.routes.draw do
 }
 
   namespace :admin do
-    get 'comments/index'
-    get 'calculations/index'
-    get 'calculations/show'
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
+    resources :users , only:[:index,:show,:edit,:update]
+    resources :tags , only:[:index, :create, :edit, :update, :destroy]
+    resources :calculations , only:[:index,:show,:destroy] do
+    resources :comments , only:[:index,:destroy],module: :calculations
+    end
     get 'homes/top'
+    
   end
 
 
  scope module: :public do
-    get 'calculations/index'
-    get 'calculations/show'
-    get 'calculations/edit'
-    get 'users/show'
-    get 'users/edit'
-    get 'homes/top'
-    get 'homes/about'
-
+    resources :homes ,only:[:top,:about]
+    resources :users, only: [:show, :edit, :update]
+    resources :calculations, only:[:index, :show, :edit,:create ,:destroy ,:update] do
+    resources :comments, only:[:create, :destroy],module: :calculations
+    resources :favorites, only:[:create, :destroy], module: :calculations
+    end
+    
+    get '/users/unsubscribe', to: 'users#unsubscribe', as: 'users_unsubscribe'
+    # patch 'users/:id' ,to: 'users#update', as: 'users_update'
+    patch '/users/withdraw', to: 'users#withdraw', as: 'users_withdraw'
     get '/', to: 'homes#top' , as: 'user_top'
     get '/about', to: 'homes#about' , as: 'user_about'
-    get '/users/unsubscribe', to: 'users#unsubscribe', as: 'users_unsubscribe'
-    patch 'users/:id' ,to: 'userss#update', as: 'users_update'
-    patch '/users/withdraw', to: 'users#withdraw', as: 'users_withdraw'
-    post '/calculations' ,to: 'calculations#create', as: 'calculations_create'
-    patch '/calculations' ,to: 'calculations#update', as: 'calculations_update'
-
+   
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

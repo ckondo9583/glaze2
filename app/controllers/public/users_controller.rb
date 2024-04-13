b862330fb586eeef4faa13adc54ba20666c36b8f
ChiakiKondo <154647391+ckondo9583@users.noreply.github.com>
2024-04-13 13:27:59 +0000

    ユーザー編集機能作成完了4/13

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,38 @@
 class Public::UsersController < ApplicationController
    before_action :authenticate_user!
-   
+
   def show
-     @calculations = current_user.calculations
+    @user = User.find(params[:id])
+    if current_user == @user || current_user?
+      @calculations = @user.calculations
+    else
+      @calculations = @user.calculations.where(release_status: 1)
+    end
   end
 
+
   def edit
+    @user = current_user
   end
+
+  def update
+    @user = User.find(params[:id])
+     if @user.update(user_params)
+       flash[:notice] = "You have updated user successfully."
+        redirect_to user_path(@user)
+     else
+       render :edit
+     end
+  end
+
+   private
+
+  def user_params
+    params.require(:user).permit(:name,:email ,:introduction)
+  end
+  
+  def current_user?
+    current_user == @user
+  end
+
 end
